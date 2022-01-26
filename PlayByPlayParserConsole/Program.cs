864d1f63b9435507395f6a72f9f0892e30dc38fd
maotterson <otterson.2@gmail.com>
2022-01-25 23:11:39 -0500

    Add project files.

---
+++ PlayByPlayParserConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.VisualBasic.FileIO;
+
+using (TextFieldParser parser = new TextFieldParser(@".\Sample Data\GameDataFBRef.csv"))
+{
+    parser.TextFieldType = FieldType.Delimited;
+    parser.SetDelimiters(",");
+    while (!parser.EndOfData)
+    {
+        //Processing row
+        string[] fields = parser.ReadFields();
+        foreach (string field in fields)
+        {
+            Console.WriteLine(field);
+            //TODO: Process field
+        }
+        Console.ReadLine();
+    }
+}
