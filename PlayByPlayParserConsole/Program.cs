a4db719ee42bf23e170f7dabf714603fb8595dd1
maotterson <otterson.2@gmail.com>
2022-01-26 00:36:42 -0500

    replace with linq query to map csv for play by play

--- PlayByPlayParserConsole/Program.cs
+++ PlayByPlayParserConsole/Program.cs
@@ -1,19 +1,47 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Microsoft.VisualBasic.FileIO;
 
-using (TextFieldParser parser = new TextFieldParser(@".\Sample Data\GameDataFBRef.csv"))
+string filePath = @".\Sample Data\GameDataFBRef.csv";
+
+//outputFileIO(filePath);
+linqMapping(filePath);
+
+void outputFileIO(string filePath)
 {
-    parser.TextFieldType = FieldType.Delimited;
-    parser.SetDelimiters(",");
-    while (!parser.EndOfData)
+    using (TextFieldParser parser = new TextFieldParser(filePath))
     {
-        //Processing row
-        string[] fields = parser.ReadFields();
-        foreach (string field in fields)
+        parser.TextFieldType = FieldType.Delimited;
+        parser.SetDelimiters(",");
+        while (!parser.EndOfData)
         {
-            Console.WriteLine(field);
-            //TODO: Process field
+            //Processing row
+            string[] fields = parser.ReadFields();
+            foreach (string field in fields)
+            {
+                Console.Write(field + " ");
+            }
         }
         Console.ReadLine();
+    }
+}
+
+void linqMapping(string filePath)
+{
+    var playData = File.ReadAllLines(filePath)
+                   .Skip(1)
+                   .Select(x => x.Split(','))
+                   .Select((x,index) => new
+                    {
+                        PlayIndex = index+1,
+                        Quarter = x[0],
+                        Time = x[1],
+                        Down = x[2],
+                        ToGo = x[3],
+                        Location = x[4],
+                        Summary = x[5]
+                   });
+    foreach(var play in playData)
+    {
+        Console.WriteLine(play.Summary);
     }
 }
