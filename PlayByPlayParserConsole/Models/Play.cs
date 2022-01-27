2171b5f3f356ba54987c24e66c2f3cbd4cfd8e7f
maotterson <otterson.2@gmail.com>
2022-01-26 22:31:16 -0500

    refactor anonymous type to new "play" model class

---
+++ PlayByPlayParserConsole/Models/Play.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayByPlayParserConsole.Models
+{
+    internal class Play
+    {
+        public int PlayIndex { get; set; }
+        public string Quarter { get; set; }
+        public string Time { get; set; }
+        public string Down { get; set; }
+        public string ToGo { get; set; }
+        public string Location { get; set; }
+        public string Summary { get; set; }
+    }
+}
--- PlayByPlayParserConsole/Program.cs
+++ PlayByPlayParserConsole/Program.cs
@@ -1,31 +1,37 @@
-﻿string filePath = @".\Sample Data\GameDataFBRef.csv";
-linqMapping(filePath);
+﻿using PlayByPlayParserConsole.Models;
 
-void linqMapping(string filePath)
+string filePath = @".\Sample Data\GameDataFBRef.csv";
+IEnumerable<Play> playList = linqMapping(filePath);
+outputPlayList(playList);
+
+/// <summary>
+/// Extract plays from csv using linq syntax
+/// </summary>
+IEnumerable<Play> linqMapping(string filePath)
 {
-    var playData = File.ReadAllLines(filePath)
+    IEnumerable<Play> playData = File.ReadAllLines(filePath)
                    .Skip(1)
                    .Select(x => x.Split(','))
-                   .Select((x,index) => new
-                    {
-                        PlayIndex = index+1,
-                        Quarter = x[0],
-                        Time = x[1],
-                        Down = x[2],
-                        ToGo = x[3],
-                        Location = x[4],
-                        Summary = x[5]
+                   .Select((x, index) => new Play
+                   {
+                       PlayIndex = index + 1,
+                       Quarter = x[0],
+                       Time = x[1],
+                       Down = x[2],
+                       ToGo = x[3],
+                       Location = x[4],
+                       Summary = x[5]
                    });
-    foreach(var play in playData)
-    {
-        // extract data from summary by type
-        string[] playStrings = play.Summary.Split(' ');
-        if(playStrings[0] == "Penalty")
-        {
-            Console.WriteLine("*");
-            continue;
-        }
-        Console.WriteLine(playStrings[0] + " " + playStrings[1]);
+    return playData;
+}
 
+/// <summary>
+/// Sample method to output some data for each play
+/// </summary>
+void outputPlayList(IEnumerable<Play> playList)
+{
+    foreach (Play play in playList)
+    {
+        Console.WriteLine(play.Time);
     }
 }
