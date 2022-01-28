fc1aa31f99f78c00b048bbbd791622c8738378a0
maotterson <otterson.2@gmail.com>
2022-01-27 22:06:38 -0500

    add tracked pass attributes to playevent

---
+++ PlayByPlayParserConsole/PlayEvent/PlayTypes/Pass/PassType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayByPlayParserConsole.PlayEvent.PlayTypes.Pass
+{
+    internal enum PassType
+    {
+        ShortLeft,
+        ShortRight,
+        ShortMiddle,
+        DeepLeft,
+        DeepRight,
+        DeepMiddle,
+    }
+}
--- PlayByPlayParserConsole/PlayEvent/PlayTypes/PassPlayEvent.cs
+++ PlayByPlayParserConsole/PlayEvent/PlayTypes/PassPlayEvent.cs
@@ -1,4 +1,5 @@
 ﻿using PlayByPlayParserConsole.Models;
+using PlayByPlayParserConsole.PlayEvent.PlayTypes.Pass;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,15 @@ namespace PlayByPlayParserConsole.PlayEvent
 {
     internal class PassPlayEvent : IPlayEvent
     {
+        //Aaron Rodgers pass complete short left to Davante Adams for 7 yards (tackle by Azeez Al-Shaair)
+
         public string PlayType { get; set; } = "Pass";
+        public int PassingYards { get; set; }
+        public string Passer { get; set; }
+        public string Target { get; set; }
+        public bool IsCompleted { get; set; }
+        public bool IsIntercepted { get; set; }
+        public string[] Tacklers { get; set; }
+        public PassType PassType { get; set; }
     }
 }
